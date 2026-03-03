1eca6ff2245f9fe27ccb3de205fc0a16ecb66e87
kevin4bmael <kevin4bmael@gmail.com>
2026-03-02 22:17:07 -0300

    add sidebar in home

---
+++ src/components/SIdebar.jsx
@@ -0,0 +1,10 @@
+export default function Sidebar(){
+    return(
+        <div>
+            <ul>
+                <li> Sobre nós</li>
+                <li>Noticias </li>
+            </ul>
+        </div>
+    )
+}
